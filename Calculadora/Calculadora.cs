76e9cc2d4256a5c5f0946d682daf4ddcb5ca4000
tc.Claudelino <tc.Claudelino@ibopemedia.kantar.com>
2019-11-12 10:21:16 -0300

    Add project files.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    {
+        public int Soma(int operador1, int operador2) => operador1 + operador2;
+        public int Subtracao(int operador1, int operador2) => operador1 - operador2;
+        public int Multiplicao(int operador1, int operador2) => operador1 * operador2;
+        public int Divisao(int dividendo, int divisor) => dividendo / divisor;
+        public (int quociente, int resto) RestoDivisao(int dividendo, int divisor) => (dividendo / divisor, dividendo % divisor);
+    }
+}
---
+++ CalculadoraTests/CalculadoraTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void Soma_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Soma(10, 20);
+            //Verifica se o resultado é igual a 30
+            Assert.AreEqual(30, resultado);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        public void RestoDivisao_DeveRetornarZero(int value)
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.RestoDivisao(12, value);
+            //Verifica se o resto da divisão é 0
+            Assert.AreEqual(0, resultado.resto);
+        }
+
+        [TestMethod]
+        public void RestoDivisao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.RestoDivisao(10, 3);
+            //Verifica se o quociente da divisão é 3 e o resto 1
+            Assert.AreEqual(3, resultado.quociente);
+            Assert.AreEqual(1, resultado.resto);
+        }
+
+        [TestMethod]
+        public void Subtracao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Subtracao(20, 10);
+            //Verifica se o resultado é igual a 10
+            Assert.AreEqual(10, resultado);
+        }
+
+        [TestMethod]
+        public void Divisao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Divisao(100, 10);
+            //Verifica se o resultado é igual a 10
+            Assert.AreEqual(10, resultado);
+        }
+
+        [TestMethod]
+        public void Multiplicao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Multiplicao(5, 2);
+            //Verifica se o resultado é igual a 10
+            Assert.AreEqual(10, resultado);
+        }
+    }
+}
